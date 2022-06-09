451f7801e11807b3f8fa9923496aa9645eaf208d
AlexeyMerkulov <alexeymerkulovv@gmail.com>
2022-06-09 19:52:30 +0300

    4. Скачивание файла с ограничением. [#144271](исправление)

--- src/main/java/ru/job4j/concurrent/thread/Wget.java
+++ src/main/java/ru/job4j/concurrent/thread/Wget.java
@@ -10,37 +10,54 @@ public class Wget implements Runnable {
     private final int speed;
     private final String nameFile;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String nameFile) {
         this.url = url;
         this.speed = speed;
-        this.nameFile = "downloadedFile.txt";
+        this.nameFile = nameFile;
     }
 
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
              FileOutputStream fileOutputStream = new FileOutputStream(nameFile)) {
-            byte[] dataBuffer = new byte[speed];
+            byte[] dataBuffer = new byte[1024];
             int bytesRead;
+            long bytesWrited = 0;
             long startTime = System.currentTimeMillis();
             while ((bytesRead = in.read(dataBuffer, 0, speed)) != -1) {
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
-                long stopTime = System.currentTimeMillis();
-                long workTime = stopTime - startTime;
-                if (workTime < 1000) {
-                    Thread.sleep(1000 - workTime);
+                bytesWrited += bytesRead;
+                if (bytesWrited >= speed) {
+                    long deltaTime = System.currentTimeMillis() - startTime;
+                    if (deltaTime < 1000) {
+                        Thread.sleep(1000 - deltaTime);
+                    }
+                    bytesWrited = 0;
+                    startTime = System.currentTimeMillis();
                 }
-                startTime = System.currentTimeMillis();
             }
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException e) {
             e.printStackTrace();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public static void validateArgsNum(int argsNum) {
+        if (argsNum != 3) {
+            throw new IllegalArgumentException("Введено неверное кол-во параметров" + System.lineSeparator()
+                    + "1. Введите адрес ссылки для скачивания файла" + System.lineSeparator()
+                    + "2. Введите желаемую скорость скачивания в байт/с" + System.lineSeparator()
+                    + "3. Введите название файла для записи на диск");
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
+        Wget.validateArgsNum(args.length);
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        String nameOfFile = args[2];
+        Thread wget = new Thread(new Wget(url, speed, nameOfFile));
         wget.start();
         wget.join();
     }
