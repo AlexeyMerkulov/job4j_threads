9c4fe53341b976d720e17ebfd44d8a9eb1e7a80c
AlexeyMerkulov <alexeymerkulovv@gmail.com>
2022-06-14 00:18:27 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/concurrent/io/ParseFile.java
@@ -0,0 +1,34 @@
+package ru.job4j.concurrent.io;
+
+import java.io.*;
+import java.util.function.Predicate;
+
+public final class ParseFile {
+    private final File file;
+
+    public ParseFile(File file) {
+        this.file = file;
+    }
+
+    public String content(Predicate<Character> filter) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(file))) {
+            int data;
+            while ((data = in.read()) > 0) {
+                char charData = (char) data;
+                if (filter.test(charData)) {
+                    sb.append(charData);
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    public String getContent() throws IOException {
+        return content(x -> true);
+    }
+
+    public String getContentWithoutUnicode() throws IOException {
+        return content(x -> x < 0x80);
+    }
+}
