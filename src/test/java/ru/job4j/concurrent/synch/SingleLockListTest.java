ebc367938d520931d4339a4b7644ca8109f7358e
AlexeyMerkulov <alexeymerkulovv@gmail.com>
2022-06-16 23:25:04 +0300

    4. ThreadSafe динамический список [#1105]

---
+++ src/test/java/ru/job4j/concurrent/synch/SingleLockListTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.concurrent.synch;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SingleLockListTest {
+
+    @Test
+    public void add() throws InterruptedException {
+        List<Integer> original = new ArrayList<>();
+        original.add(10);
+        original.add(20);
+        SingleLockList<Integer> list = new SingleLockList<>(original);
+        Thread first = new Thread(() -> list.add(1));
+        Thread second = new Thread(() -> list.add(2));
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        Set<Integer> rsl = new TreeSet<>();
+        list.iterator().forEachRemaining(rsl::add);
+        assertThat(rsl, is(Set.of(1, 2, 10, 20)));
+    }
+}
