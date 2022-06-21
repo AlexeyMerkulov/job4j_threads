1c05e037f5434011abfab97afc2b8886aed180ad
AlexeyMerkulov <alexeymerkulovv@gmail.com>
2022-06-21 23:00:17 +0300

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/ru/job4j/concurrent/wait/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/concurrent/wait/SimpleBlockingQueueTest.java
@@ -2,6 +2,9 @@ package ru.job4j.concurrent.wait;
 
 import org.junit.Test;
 
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
+
 import static org.junit.Assert.*;
 
 public class SimpleBlockingQueueTest {
@@ -31,5 +34,35 @@ public class SimpleBlockingQueueTest {
         consumer.start();
         producer.join();
         consumer.join();
+    }
+
+    @Test
+    public void whenExecute2Threads() throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(7);
+        Thread consumer = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                try {
+                    queue.poll();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+        Thread producer = new Thread(() -> {
+            try {
+                for (int i = 0; i < 6; i++) {
+                    queue.offer(i);
+                }
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertEquals(5, queue.poll().intValue());
     }
 }
