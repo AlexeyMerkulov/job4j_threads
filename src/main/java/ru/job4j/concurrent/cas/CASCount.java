dac1ff391d18b573f6d8973dcc1cca2888e8cb7c
AlexeyMerkulov <alexeymerkulovv@gmail.com>
2022-06-23 17:42:26 +0300

    0. CAS - операции [#6859]

---
+++ src/main/java/ru/job4j/concurrent/cas/CASCount.java
@@ -0,0 +1,23 @@
+package ru.job4j.concurrent.cas;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+@ThreadSafe
+public class CASCount {
+    private final AtomicReference<Integer> count = new AtomicReference<>();
+
+    public void increment() {
+        int current;
+        int newValue;
+        do {
+            current = count.get();
+            newValue = current + 1;
+        } while (!count.compareAndSet(current, newValue));
+    }
+
+    public int get() {
+        return count.get();
+    }
+}
