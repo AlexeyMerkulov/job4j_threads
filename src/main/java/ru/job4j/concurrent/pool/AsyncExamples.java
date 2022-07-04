2341bf3f8257ea961b02b8a82f56e8f5dda50097
AlexeyMerkulov <alexeymerkulovv@gmail.com>
2022-07-04 18:55:55 +0300

    Async(CompletableFuture) examples

---
+++ src/main/java/ru/job4j/concurrent/pool/AsyncExamples.java
@@ -0,0 +1,147 @@
+package ru.job4j.concurrent.pool;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+public class AsyncExamples {
+
+    private static void iWork() throws InterruptedException {
+        int count = 0;
+        while (count < 10) {
+            System.out.println("Вы: Я работаю");
+            TimeUnit.SECONDS.sleep(1);
+            count++;
+        }
+    }
+    //runAsync запускает асинхронно задачу и ничего не возвращает
+    public static CompletableFuture<Void> goToTrash() {
+        return CompletableFuture.runAsync(
+                () -> {
+                    System.out.println("Сын: Мам/Пам, я пошел выносить мусор");
+                    try {
+                        TimeUnit.SECONDS.sleep(5);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Сын: Мам/Пап, я вернулся!");
+                }
+        );
+    }
+    //supplyAsync запускает асинхрнно задачу и возвращает значение
+    public static CompletableFuture<String> buyProduct(String product) {
+        return CompletableFuture.supplyAsync(
+                () -> {
+                    System.out.println("Сын: Мам/Пам, я пошел в магазин");
+                    try {
+                        TimeUnit.SECONDS.sleep(5);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Сын: Мам/Пап, я купил " + product);
+                    return product;
+                }
+        );
+    }
+    //Запустить в main
+    public static void runAsyncExample() throws Exception {
+        CompletableFuture<Void> gtt = goToTrash();
+        iWork();
+    }
+    //Запустить в main
+    public static void supplyAsyncExample() throws Exception {
+        CompletableFuture<String> bm = buyProduct("Молоко");
+        iWork();
+        System.out.println("Куплено: " + bm.get());
+    }
+    //Работа thenRun - выполнение задачи после завершения gtt
+    public static void thenRunExample() throws Exception {
+        CompletableFuture<Void> gtt = goToTrash();
+        gtt.thenRun(() -> {
+            int count = 0;
+            while (count < 3) {
+                System.out.println("Сын: я мою руки");
+                try {
+                    TimeUnit.MILLISECONDS.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                count++;
+            }
+            System.out.println("Сын: Я помыл руки");
+        });
+        iWork();
+    }
+    //thenAccept имеет доступ к результату CompletableFuture
+    public static void thenAcceptExample() throws Exception {
+        CompletableFuture<String> bm = buyProduct("Молоко");
+        bm.thenAccept((product) -> System.out.println("Сын: Я убрал " + product + " в холодильник "));
+        iWork();
+        System.out.println("Куплено: " + bm.get());
+    }
+    //thenApply преобразовывает рез-т CompletableFuture, рез-т преобразования станет доступным при вызове get()
+    public static void thenApplyExample() throws Exception {
+        CompletableFuture<String> bm = buyProduct("Молоко")
+                .thenApply((product) -> "Сын: я налил тебе в кружку " + product + ". Держи.");
+        iWork();
+        System.out.println(bm.get());
+    }
+    //для совмещения двух объектов CompletableFuture можно использовать thenCompose(), thenCombine()
+    //thenCompose() используется, если действия зависимы, т.е. сначала выполнится одно а только потом другое
+    public static void thenComposeExample() throws Exception {
+        CompletableFuture<String> result = goToTrash().thenCompose(a -> buyProduct("Milk"));
+        result.get();
+    }
+    //thenCombine() используется, если действия могут быть выполнены независимо друг от друга.
+    // Причем в качестве второго аргумента, нужно передавать BiFunction – функцию,
+    // которая преобразует результаты двух задач во что-то одно
+    public static void thenCombineExample() throws Exception {
+        CompletableFuture<String> result = buyProduct("Молоко")
+                .thenCombine(buyProduct("Хлеб"), (r1, r2) -> "Куплены " + r1 + " и " + r2);
+        iWork();
+        System.out.println(result.get());
+    }
+
+    public static CompletableFuture<Void> washHands(String name) {
+        return CompletableFuture.runAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(name + ", моет руки");
+        });
+    }
+    //allOf() возвращает ComputableFuture<Void>, при этом обеспечивает выполнение всех задач
+    public static void allOfExample() throws Exception {
+        CompletableFuture<Void> all = CompletableFuture.allOf(
+                washHands("Папа"), washHands("Мама"),
+                washHands("Ваня"), washHands("Боря")
+        );
+        TimeUnit.SECONDS.sleep(3);
+    }
+
+    public static CompletableFuture<String> whoWashHands(String name) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return name + ", моет руки";
+        });
+    }
+    //anyOf()  возвращает ComputableFuture<Object>. Результатом будет первая выполненная задача
+    public static void anyOfExample() throws Exception {
+        CompletableFuture<Object> first = CompletableFuture.anyOf(
+                whoWashHands("Папа"), whoWashHands("Мама"),
+                whoWashHands("Ваня"), whoWashHands("Боря")
+        );
+        System.out.println("Кто сейчас моет руки?");
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println(first.get());
+    }
+
+    public static void main(String[] args) throws Exception {
+        anyOfExample();
+    }
+}
